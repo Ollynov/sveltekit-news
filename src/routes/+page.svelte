321563d52790bb6787a98d862237ccb3e0837c55
Ollynov <ollynov@gmail.com>
2023-04-15 21:50:25 -0700

    displaying data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,11 @@
 	// import welcome from '$lib/images/svelte-welcome.webp';
 	// import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import Container from './layout/Container.svelte'
+	import ArticlePreview from './articles/ArticlePreview.svelte'
+	export let data;
 
+	$: articles = data.articles
+	console.log('ok here is data: ', data)
 </script>
 
 <svelte:head>
@@ -24,20 +28,45 @@
 			</h1>
 		</Container>
 		<div class="divide-y divide-slate-100 sm:mt-4 lg:mt-8 lg:border-t lg:border-slate-100">
-			<!-- {episodes.map((episode) => (
-				<EpisodeEntry key={episode.id} episode={episode} />
-			))} -->
+			{#each articles as article}
+				<!-- <ArticlePreview article={article} /> -->
+				<article
+					aria-labelledby={`article-${article?.id}-title`}
+					class="py-10 sm:py-12"
+				>
+					<!-- <Container>
+						<div class="flex flex-col items-start">
+							<div class="text text-3xl">
+								hi
+							</div>
+							<h2
+								id={`article-${article?.id}-title`}
+								class="mt-2 text-lg font-bold text-slate-900"
+							>
+								<a href={`/${article?.id}`}>{article?.title}</a>
+							</h2>
+							<p class="mt-1 text-base leading-7 text-slate-200">
+								{article?.description}
+							</p>
+							<p  class="flex items-center text-sm font-bold leading-6 text-pink-500 hover:text-pink-700 active:text-pink-900 mt-4">
+								Read More
+							</p>
+						</div>
+					</Container> -->
+					<ArticlePreview article={article}/>
+				</article>
+			{/each}
 		</div>
 	</div>
 </section>
 
 <style lang="postcss">
-	section {
+	/* section {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
-	}
+	} */
 
 </style>
---
+++ src/routes/articles/ArticlePreview.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Container from './../layout/Container.svelte'
+  export let article
+  // let date = new Date(article?.published)
+
+</script>
+  
+
+<article
+  aria-labelledby={`article-${article?.id}-title`}
+  class="py-10 sm:py-12"
+>
+  <Container>
+    <div class="flex flex-col items-start">
+      <h2
+        id={`article-${article?.id}-title`}
+        class="mt-2 text-lg font-bold text-slate-900"
+      >
+        <a href={`/${article?.id}`}>{article?.title}</a>
+      </h2>
+
+      <!-- <FormattedDate
+        date={date}
+        class="order-first font-mono text-sm leading-7 text-slate-500"
+      /> -->
+      <p class="mt-1 text-base leading-7 text-slate-700">
+        {article?.description}
+      </p>
+      <p  class="flex items-center text-sm font-bold leading-6 text-pink-500 hover:text-pink-700 active:text-pink-900 mt-4">
+        Read More
+      </p>
+    </div>
+  </Container>
+</article>
+
+<style lang="postcss">
+	/* .text {
+		color: green;
+	} */
+
+</style>
