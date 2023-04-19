a3501fedcf469c73f18bcf897f7dae4c4591eb74
Ollynov <ollynov@gmail.com>
2023-04-19 00:11:19 -0700

    added more unitary styles for images

--- src/routes/articles/ArticlePreview.svelte
+++ src/routes/articles/ArticlePreview.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Container from './../layout/Container.svelte'
   export let article
   // let date = new Date(article?.published)
 
@@ -10,16 +9,17 @@
   aria-labelledby={`article-${article?.id}-title`}
   class="py-10 sm:py-12"
 >
-  <Container>
-    <div class="flex flex-row">
-      <div class="w-40">
+  <!-- <Container> -->
+    <div class="flex flex-row w-8/12 ml-28">
+      <div class="mr-8 w-1/3">
         <img
           src={article.urlToImage}
           alt=""
+          class="w-full"
         />
       </div>
 
-      <div class="flex flex-col items-start">
+      <div class="flex flex-col items-start w-2/3">
         <h2
           id={`article-${article?.id}-title`}
           class="mt-2 text-lg font-bold text-slate-900"
@@ -34,13 +34,13 @@
         <p class="mt-1 text-base leading-7 text-slate-700">
           {article?.description}
         </p>
-        <p  class="flex items-center text-sm font-bold leading-6 text-pink-500 hover:text-pink-700 active:text-pink-900 mt-4">
+        <p  class="flex cursor-pointer items-center text-sm font-bold leading-6 text-pink-500 hover:text-pink-700 active:text-pink-900 mt-4">
           Read More
         </p>
       </div>
     </div>
 
-  </Container>
+  <!-- </Container> -->
 </article>
 
 <style lang="postcss">
