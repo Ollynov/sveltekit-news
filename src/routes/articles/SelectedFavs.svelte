dc08918b3f7a926782f5ace974df42ed0d11b4af
Ollynov <ollynov@gmail.com>
2023-04-19 22:57:10 -0700

    styled the favorites section a bit

--- src/routes/articles/SelectedFavs.svelte
+++ src/routes/articles/SelectedFavs.svelte
@@ -1,8 +1,19 @@
 
 <script>
   import TinyWave from '../utility-components/icons/TinyWave.svelte';
+  import remove from '$lib/images/remove-icon.svg';
+
   import { favorites } from './stores';
 
+  const removeFav = (url) => {
+    
+    favorites.update(current => {
+      if (current[url]) {
+        delete current[url]
+      } 
+      return current;
+    });
+  }
 </script>
 
 <section class="mt-10">
@@ -14,9 +25,19 @@
   <ul class='mt-2 text-base leading-7 text-slate-700' >
     {#each Object.entries($favorites) as [url, title]}
 
-      <li>
-        <a class="text-pink-500 cursor-pointer" href={url}> {title}</a>
-       
+      <li class="mt-4">
+        <a class="text-slate-700 cursor-pointer text-sm" href={url}> {title}</a>
+        <div class="flex flex-row mt-1">
+          <button class="flex cursor-pointer items-center text-sm font-bold leading-6 text-pink-500 hover:text-pink-700 active:text-pink-900">
+            Read More
+          </button>    
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <div on:click={removeFav(url)}  class="ml-3 flex flex-row cursor-pointer items-center text-sm font-bold leading-6 text-pink-500 hover:text-pink-700 active:text-pink-900">
+              <img class="w-5 ml-0.5 " fill="#db2777" src={remove} alt="favorite" />
+          </div>
+        </div>
+      
+
       </li>
     {/each}
   </ul>
